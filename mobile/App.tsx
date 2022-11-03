61bb4257bc1c7997e0f54628ddd78abca76ac4f8
Edson Nascimento <edsnascimento89@gmail.com>
2022-11-03 15:00:17 -0300

    [new]: initial commit

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/styles/theme';
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SingIn';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor= "null"
+        translucent
+      />
+        { fontsLoaded ? <SignIn /> : <Loading /> }
+    </NativeBaseProvider>
+  );
+}
---
+++ mobile/src/screens/SingIn.tsx
@@ -0,0 +1,11 @@
+import { Text, Center } from "native-base";
+
+export function SignIn() {
+    return (
+        <Center flex={1} bgColor="gray.900">
+        <Text color="white" fontSize={24} fontFamily="heading">
+          SignIn
+        </Text>
+        </Center>
+    )
+}
