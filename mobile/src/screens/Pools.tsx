af006ccd0917c50a8da68b5aaf82ac6d3715505b
Edson Nascimento <edsnascimento89@gmail.com>
2023-01-11 09:55:52 -0300

    [new] : integrating backend into pools

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,24 +1,70 @@
-import { Icon, VStack } from 'native-base';
+import { useCallback, useState } from 'react'
+import { Icon, useToast, VStack, FlatList } from 'native-base';
 import { Octicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+
+import { api } from '../services/api';
 
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
+import { Loading } from '../components/Loading';
+import { PoolCard, PoolCardProps } from '../components/PoolCard';
+import { EmptyPoolList } from '../components/EmptyPoolList';
 
 export function Pools() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [pools, setPools] = useState<PoolCardProps[]>([]);
+
     const { navigate } = useNavigation();
+    const toast = useToast();
+
+    async function fetchPools() {
+        try {
+            setIsLoading(true);
+
+            const response = await api.get('/pools');
+            setPools(response.data.pools);
+
+        } catch (error) {
+            console.log(error);
+
+            toast.show({
+                title: 'Não foi possível  carregar os bolões',
+                placement: 'top',
+                bgColor: 'red.500'
+            });
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useFocusEffect(useCallback(() => {
+        fetchPools();
+    }, []));
 
     return (
         <VStack flex={1} bgColor="gray.900">
             <Header title="Meus bolões" />
 
             <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
-                <Button 
+                <Button
                     title="BUSCAR BOLÃO POR CÓDIGO"
                     leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
                     onPress={() => navigate('find')}
                 />
             </VStack>
+            {
+                isLoading ? <Loading /> :
+                <FlatList
+                    data={pools}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <PoolCard data={item} />}
+                    px={5}
+                    showsVerticalScrollIndicator={false}
+                    _contentContainerStyle={{ pb: 10 }}
+                    ListEmptyComponent={() => <EmptyPoolList />}
+                />
+            }
         </VStack>
     )
 }
